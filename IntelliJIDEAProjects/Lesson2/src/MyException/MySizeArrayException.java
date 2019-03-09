b1eec6f34f54477ff741d31ba0abf993a694675e
logg1n <46058751+logg1n@users.noreply.github.com>
2019-03-10 01:39:44 +0300

    Delete MySizeArrayException.java

--- IntelliJIDEAProjects/Lesson2/src/MyException/MySizeArrayException.java
+++
@@ -1,9 +0,0 @@
-package MyException;
-
-public class MySizeArrayException extends MyArrayException {
-    public MySizeArrayException() {
-        super("\nРазмерность массива не в входит в допустимое значение!");
-    }
-
-}
-
