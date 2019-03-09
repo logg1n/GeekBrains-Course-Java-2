b5543cafd0dc9975a2534ba72796dc48fae068af
logg1n <46058751+logg1n@users.noreply.github.com>
2019-03-10 01:41:35 +0300

    Delete People.java

--- IntelliJIDEAProjects/Lesson1/src/Sportsman/People/People.java
+++
@@ -1,9 +0,0 @@
-package Sportsman.People;
-
-import Sportsman.Sportsman;
-
-public abstract class People extends Sportsman{
-    public People(int distRun, int distJump, int distSwim) {
-        super(distRun, distJump, distSwim);
-    }
-}
