3589a2d627621d43d2797c4d47376a9c71a4d294
log1n <loggiin13@gmail.com>
2019-03-16 23:17:54 +0300

    Lesson6

---
+++ IntelliJIDEAProjects/Lesson6/src/Client.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.net.Socket;
+
+public class Client extends User {
+    @Override
+    public void start() throws IOException {
+        Socket socket = new Socket ("localhost", getPORT ( ));
+        if (socket.isConnected ( )) {
+            System.out.println ("Client connected");
+            setSocket (socket);
+            init ( );
+        }
+        else{
+            closed ();
+        }
+    }
+
+    public static void main(String[] args) throws IOException{
+        Client client = new Client ();
+        client.start ();
+    }
+}
---
+++ IntelliJIDEAProjects/Lesson6/src/Server.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server extends User{
+    private static ServerSocket server;
+    private static Socket socket;
+
+    @Override
+    public void start() throws IOException {
+        server = new ServerSocket (4040);
+        if (!server.isClosed ( )) {
+            System.out.println ("Server started ...");
+            socket = server.accept ( );
+            System.out.println ("user connected ...");
+            setSocket (socket);
+            init ();
+        } else {
+            closed ( );
+        }
+    }
+
+    public static void main(String[] args) throws IOException{
+        Server server = new Server ( );
+        server.start ( );
+    }
+}
