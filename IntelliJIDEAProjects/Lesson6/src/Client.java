961bb0944d5f9863846be51baf2b7655180162c3
log1n <loggiin13@gmail.com>
2019-03-18 10:51:00 +0300

    Lesson6

--- IntelliJIDEAProjects/Lesson6/src/Client.java
+++ IntelliJIDEAProjects/Lesson6/src/Client.java
@@ -1,23 +1,80 @@
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
 
-public class Client extends User {
- 
-    @Override
-    public void start() throws IOException {
-        Socket socket = new Socket ("localhost", getPORT ( ));
-        if (socket.isConnected ( )) {
-            System.out.println ("Client connected");
-            setSocket (socket);
-            init ( );
+public abstract class Client {
+    private static int PORT = 4040;
+    public String nickname;
+    private Socket socket;
+    private DataInputStream input;
+    private DataOutputStream output;
+    private BufferedReader reader;
+
+    public static int getPORT() {
+        return PORT;
+    }
+
+    public static void setPORT(int PORT) {
+        Client.PORT = PORT;
+    }
+
+    public void init() throws IOException {
+        input = new DataInputStream (socket.getInputStream ( ));
+        output = new DataOutputStream (socket.getOutputStream ( ));
+        reader = new BufferedReader (new InputStreamReader (System.in));
+
+        Thread in = new Thread (() -> {
+            while (true) {
+                try {
+                    String string = input.readUTF ( );
+                    System.out.println (string);
+                } catch (IOException ex) {
+                    closed ( );
+                    System.out.println ("Error print");
+                    break;
+                }
+            }
+        });
+
+        Thread out = new Thread (() -> {
+            while (true) {
+                try {
+                    String string = reader.readLine ( );
+                    output.writeUTF (nickname + ": " + string);
+                    output.flush ( );
+                }catch (IOException ex1) {
+                    closed ( );
+                    System.out.println ("Error send");
+                    break;
+                }
+            }
+        });
+        try {
+            in.start ( );
+            out.start ( );
+            in.join ( );
+            out.join ( );
+        } catch (InterruptedException inex) {
+            closed ( );
         }
-        else{
-            closed ();
+        closed ( );
+    }
+
+    public void closed() {
+        try {
+            input.close ( );
+            output.close ( );
+            reader.close ( );
+            socket.close ( );
+            System.exit (0);
+        } catch (IOException ex) {
+            System.exit (-1);
         }
     }
 
-    public static void main(String[] args) throws IOException{
-        Client client = new Client ();
-        client.start ();
+    protected void setSocket(Socket socket) {
+        this.socket = socket;
     }
+
+    public abstract void start() throws IOException;
 }
+
